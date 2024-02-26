ecc2ad5b6e20d3e8042d270d51d4f27270cef4a7
Xeon Bron <89444265+AlmightyX20@users.noreply.github.com>
2024-02-26 23:00:05 +0800

    Add files via upload

---
+++ AdminHeader.php
@@ -0,0 +1,16 @@
+<header>
+        <div class="Header-page">   
+            <div class="Logo">
+                <span class="Inbilla"><a href="Home.php"><img src="http://localhost/InbillaDraft/Images/Inbilla Logo.png" width="60" height="65"></a></span>
+                <div class="InbillaText">
+                    <h1>INBILLA</h1>
+                    <h5>TRADING</h5>
+                </div>
+            </div>
+            <h3>Admin Dashboard</h3>
+        </div>
+
+        <div class="Profile">
+            <a href="Login.php"><i class="fas fa-user"></i></a>
+        </div>    
+    </header>
