8a26b8377d2dcac998e2fe7f7533a14a4dff74f2
eliecruz <130725623+eliecruz@users.noreply.github.com>
2024-02-27 18:49:34 +0800

    Update Profile.php

--- Profile.php
+++ Profile.php
@@ -1,3 +1,40 @@
+<?php
+    session_start();
+    if(isset($_SESSION['user_id'])) {
+
+?>
+
+
+<?php
+    include('connect.php');
+    $customerID = $_SESSION['user_id'];
+            
+
+
+            if(isset($_POST['update'])){
+
+                $fName = $_POST['first-Name'];
+                $lName = $_POST['last-Name'];
+                $email = $_POST['email'];
+                $contactNum = $_POST['Mobile-Num'];
+                $gender = $_POST['Gender'];
+
+                $query = "UPDATE Customers SET
+                            first_Name = '$fName',
+                            last_Name = '$lName',
+                            email_Add = '$email',
+                            contact_Num = '$contactNum',
+                            gender = '$gender' WHERE customer_ID = '$customerID'";
+                $result = sqlsrv_query($conn, $query);
+
+                $query = "SELECT * FROM Customers WHERE customer_ID = '$customerID'";
+                $result = sqlsrv_query($conn, $query);
+                $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC);
+            }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +47,8 @@
 </head>
 <body>
     <?php
-    include('Header.php')
+    include('Header.php');
+  
     ?>
 <div class="Profile-Container">
     <div class="SideMenu-Container">
@@ -30,54 +68,118 @@
             </ul>
         </div>
         <div class="Button-Section">
-        <button class="Logout-Button">Log out</button>
+        <form action="backend/logout.php">
+            <button class="Logout-Button" type="submit" value>Logout</button>
+        </form>
         </div>
     </div>
 
     <div class="Main-Container">
-        <h1>Account Informartion</h1>
+        <h1>Account Information</h1>
         <hr>
-        <form class="User-Info">
+
+        <?php
+
+        $customer_ID = $_SESSION['user_id'];
+        
+        
+        $query = "SELECT * FROM Customers WHERE customer_ID = '$customer_ID'";
+        $result = sqlsrv_query($conn, $query);
+        $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC);
+        
+
+        
+        
+        ?>
+
+
+        <form class="User-Info" action="Profile.php" method='post'>
+
+            <?php
+            
+                //$row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC);
+
+            ?>
+
             <div class="Name-fields">
                 <div class="Label-Input">
                     <label for="first-Name">First Name</label>
-                    <input type="text" id="first-Name" placeholder="First Name">
+                    <input type="text" required name="first-Name"<?php if(isset($_POST['edit'])){ } else {?> disabled <?php }  ?>  value = "<?php echo $row['first_Name']; ?>" onchange="myFunction(this.value)" placeholder="First Name">
                 </div>
                 <div class="Label-Input">
                     <label for="last-Name">Last Name</label>
-                    <input type="text" id="last-Name" placeholder="Last Name">
+                    <input type="text" required name="last-Name" <?php if(isset($_POST['edit'])){ } else {?> disabled <?php } ?> value = "<?php echo $row['last_Name']?>" onchange="myFunction(this.value)" placeholder="Last Name">
                 </div>
             </div>
 
                     <label for="email">Email Address</label>
-                    <input type="text" id="email" placeholder="Email">
+                    <input type="text" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" required name="email" <?php if(isset($_POST['edit'])){ } else {?> disabled <?php } ?> value = <?php echo $row['email_Add']?> placeholder="Email">
 
             <div class="Id-Field">
                 <div class="Label-Input">
                     <Label for="Mobile-Num">Mobile Number</Label>
-                    <input type="text" id="Mobile-Num" placeholder="Mobile Number">
+                    <input type="tel" pattern="[0]{1}[9]{1}[0-9]{9}" name="Mobile-Num" required placeholder="Mobile Number" <?php if(isset($_POST['edit'])){ } else {?> disabled <?php } ?> value=<?php if (isset($row['contact_Num'])) {
+                                                                            echo $row['contact_Num'];
+                                                                            }
+                                                                            ?> >
                 </div>
                 <div class="Label-Input">
                     <Label for="Acc-ID">Account ID</Label>
-                    <input type="text" id="Acc-ID" placeholder="Account ID">
+                    <input type="text" id="Acc-ID" disabled value = <?php if (isset($row['customer_ID'])) {
+                                                                            echo $row['customer_ID'];
+                                                                            }
+                                                                            else {
+                                                                            ?> placeholder="Account ID"
+                                                                            <?php
+                                                                            }
+                                                                            ?>  >
                 </div>
             </div>
 
             <div class="BdayGender-Field">
                 <div class="Label-Input">
                     <Label for="Birthday">Birthday</Label>
-                    <input type="text" id="Birthday" placeholder="Birthday">
+                    <input type="text" id="Birthday"placeholder="Birthday" disabled value=<?php if (isset($row['gender'])) {
+                                                                            echo $row['gender'];
+                                                                            }
+                                                                            ?> >
                 </div>
                 <div class="Label-Input">
                     <Label for="Gender">Gender</Label>
-                    <input type="text" id="Gender" placeholder="Gender">
+                    <input type="text" name="Gender" required placeholder="Gender" <?php if(isset($_POST['edit'])){ } else {?> disabled <?php } ?> value=<?php if (isset($row['gender'])) {
+                                                                            echo $row['gender'];
+                                                                            }
+                                                                            ?> >
                 </div>
+            </div>
+            <div class="Buttons">
+            
+                                                                        
+            <?php
+                if(isset($_POST['edit'])){
+                    ?>
+                    <button class="Edit" type="submit" name="update" value="update">Update</button>
+                    <?php
+                }
+                else if(isset($_POST['update'])) {
+                    ?>
+                    <button class="Edit" type="submit" name="edit" value="edit">Edit Profile</button>
+                    <?php
+                }
+                else {
+                    ?>
+                    <button class="Edit" type="submit" name="edit" value="edit">Edit Profile</button>
+                    <?php
+                }
+            ?>                                                                   
+           
             </div>
         </form>
 
         <div class="Buttons">
-        <a href="EditProfile.php" class="ChangePass">Change Password</a>
-        <button class="Edit">Edit Profile</button>
+            <form action="ChangePassword.php" method="post">
+                <button class="ChangePass" name="change" >Change Password</button>
+            </form>
         </div>
 
     </div>
@@ -91,3 +193,18 @@
 
 
 </html>
+<?php 
+    }
+    else {
+
+      ?>
+      <script type="text/javascript">
+      window.location.href = 'Home.php';
+      </script>
+
+      <?php 
+
+    }
+
+
+?>
