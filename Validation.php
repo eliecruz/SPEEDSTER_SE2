09807efc7700a7d95d181951d79f51f2420ef504
eliecruz <130725623+eliecruz@users.noreply.github.com>
2024-02-27 18:50:23 +0800

    Update Validation.php

--- Validation.php
+++ Validation.php
@@ -1,3 +1,62 @@
+<?php
+
+session_start();
+if(isset($_SESSION['email'])) {
+    if(isset($_POST['sendCode'])){
+        require "Mail/phpmailer/PHPMailerAutoload.php";
+        
+        $otp = rand(100000,999999);
+        $_SESSION['otp'] = $otp;
+        $mail = new PHPMailer;
+
+        $mail->isSMTP();
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 587;
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'tls';
+
+        $mail->Username = 'inbillatrading2007@gmail.com';
+        $mail->Password = 'unax nggw foya unev';
+
+        $mail->setFrom('inbillatrading2007@gmail.com', 'OTP Verification');
+        $mail->addAddress($_SESSION['email']);
+
+        $mail->isHTML(true);
+        $mail->Subject = "Verification Code";
+        $mail->Body = "
+        <p>Dear Customer, </p> <p> Thank you for registering. </p> <h3>Your verification OTP code is $otp <br></h3>
+        <br><br>
+        <p>Looking forward to serve you,</p>
+        <b>Inbilla Trading</b>
+        ";
+
+        // Attempt to send the email
+        if (!$mail->send()) {
+        // Handle failure
+        ?>
+        <script> alert($mail->ErrorInfo); </script>
+        <?php
+
+        } else {
+            
+        }
+    }
+
+
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,13 +76,15 @@
     <div class="Valid-Section">
         <div class="Valid-Container">
             <h2>Email Verification</h2>
-            <form>
+            <form action="backend/auth.php" method="post">
                 <label for="code">Enter Verification Code</label>
-                <input type="text" id="inp-code" placeholder="Code" required>
-     
-                <button type="submit">Submit Code</button>
+                <input type="text" id="inp-code" name="otp" placeholder="Code" required>
+                <button type="submit" name="validation" >Submit Code</button>
             </form>
-            <p class="bottom-text"><a href="">Resend Code</a></p>
+            <form action="" method="post">
+                <button type="submit" name="sendCode" >Send Code</button>
+            </form>
+            <p class="bottom-text"><a href="Signup.php">Back to Signup</a></p>
         </div>
     </div>
 
@@ -35,3 +96,17 @@ include('Footer.php');
 </body>
 
 </html>
+<?php
+}
+
+else {
+
+    ?>
+    <script type="text/javascript">
+    window.location.href = 'Home.php';
+    </script>
+
+    <?php 
+
+  }
+?>
