314c847335452274e1d8b1d81a4ecbbbc278db22
eliecruz <130725623+eliecruz@users.noreply.github.com>
2024-02-27 18:49:57 +0800

    Update Signup.php

--- Signup.php
+++ Signup.php
@@ -1,3 +1,21 @@
+<?php
+session_start();
+if(isset($_SESSION['user_id'])){
+    ?>
+      <script type="text/javascript">
+      window.location.href = 'Home.php';
+      </script>
+
+      <?php 
+}
+else {
+
+    session_unset();
+    session_destroy();
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,26 +35,27 @@
     <div class="Signup-Section">
         <div class="Signup-Container">
             <h2>Sign Up</h2>
-            <form>
+            <form action ="backend/auth.php" method = "POST">
 
                 <div class="name-fields">
                     <div>
                         <label for="first-name">First Name</label>
-                        <input type="text" id="first-name" placeholder="First Name" required>
+                        <input type="text" name="first-name" placeholder="First Name" required>
                     </div>
                     <div>
                         <label for="last-name">Last Name</label>
-                        <input type="text" id="last-name" placeholder="Last Name" required>
+                        <input type="text" name="last-name" placeholder="Last Name" required>
                     </div>
                 </div>
             
                 <label for="email">Email Address</label>
-                <input type="text" id="email" placeholder="Email" required>
+                <input type="text" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" name="email" placeholder="Email" required>
             
                 <label for="password">Password</label>
-                <input type="password" id="password" placeholder="Password" required>
+                <input type="password" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" name="password" placeholder="Password" required>
+                <span class="password-info">It must be a combination of minimum 8 letters(uppercase and lowercase), numbers, and symbols.</span>
             
-                <button type="submit"><i class="fa-solid fa-plus"></i>Create Account</button>
+                <button type="submit" name = "signup_form" ><i class="fa-solid fa-plus"></i>Create Account</button>
                 <button class="google-btn"><i class="fa-brands fa-google"></i> Sign up with Google</button>
                 
             </form>
@@ -55,3 +74,7 @@ include('Footer.php');
 </body>
 
 </html>
+<?php 
+    }
+   ?>
+
