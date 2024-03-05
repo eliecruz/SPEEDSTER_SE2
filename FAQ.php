09dd1809ab872262f9e333dc9a3b55a03e4f0dc4
Ej0007 <108266580+Ej0007@users.noreply.github.com>
2024-03-05 22:14:24 +0800

    Add files via upload

---
+++ FAQ.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQ</title>
+    <link rel="stylesheet" href="Header&Footer.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+    <link rel="stylesheet" href="FAQ.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php
+    include('Header.php');
+    ?>
+
+    <div class="Breadcrumb-Nav">
+        <ol>
+            <li><a href="Home.php">Home</a></li>
+            <li>FAQ</li>
+        </ol>
+    </div>
+
+    <div class="faq">
+        <div class="text">
+            <h1 id="FAQ">Frequently Asked Questions</h1>
+            <p>Welcome to our FAQ page. Here, we've compiled answers to the most common questions our users ask. If you can't find the information you're looking for, feel free to <a href="">contact us</a>.</p>
+
+            <h2>General Questions</h2>
+            <h3>Q: What is Inbilla Trading?</h3>
+            <p>A: Inbilla Trading is a wholesale retailer company of various products such as printers, inks, toners, and cartridges. Not only that, but we also offer office supplies and computer accessories.</p>
+
+            <h3>Q: How do I contact customer support?</h3>
+            <p>A: You can reach our customer support team by email, phone, or through our contact form.</p>
+
+            <h2>Ordering and Payment</h2>
+            <h3>Q: How do I place an order?</h3>
+            <p>A: Follow these steps to place an order:</p>
+            <p>[Step 1] Make sure to create an account first and then log-in.</p>
+            <p>[Step 2] Go to the Products tab that can be found on the navigation bar on top of the webpage.</p>
+            <p>[Step 3] Click a product and there you can see a "Buy Now" button to purchase a product.</p>
+
+            <h3>Q: What payment methods do you accept?</h3>
+            <p>A: We accept over-the-counter payment and through GCash by scanning the QR code once you purchase an item.</p>
+
+            <h2>Shipping and Delivery</h2>
+            <h3>Q: What are the shipping options?</h3>
+            <p>A: We offer personal delivery shipping and through pick-up on our main office.</p>
+
+            <h2>Returns and Refunds</h2>
+            <h3>Q: What is your return policy?</h3>
+            <p>A: Our return policy can be found <a href="Refund&Return.php">here</a>.</p>
+
+            <h3>Q: How do I initiate a return?</h3>
+            <p>A: To initiate a return, please follow the instructions on our <a href="Refund&Return.php">returns</a> page.</p>
+
+            <h2>Account and Profile Management</h2>
+            <h3>Q: How do I create an account?</h3>
+            <p>A: To create an account, click on <a href="Signup.php">Sign Up</a> and follow the registration process.</p>
+
+            <h3>Q: How can I reset my password?</h3>
+            <p>A: If you forget your password, you can reset it <a href="Profile.php"></a>here.</p>
+
+            <h2>Privacy and Security</h2>
+            <h3>Q: How is my personal information protected?</h3>
+            <p>A: We take the protection of your personal information seriously. For details, refer to our <a href="Privacy.php">privacy policy</a></p>
+
+            <h2>Contact Us</h2>
+            <p>If you still have questions or need further assistance, please <a href="Contacts.php">contact us</a> directly. Our customer support team is here to help.</p>
+            <p>Thank you for choosing Inbilla Trading!</p>
+
+
+
+
+
+        </div>
+       
+    </div>
+
+    <?php
+    include('Footer.php');
+    ?>
+</body>
+</html>
